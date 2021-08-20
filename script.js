40f7a19b0110ef8a6a43e24162f2ab608f3e31e4
ivanovsemyon <sivanov.exceedteam@gmail.com>
2021-08-20 16:05:59 +0300

    rewiev

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const HEADERS = {
+  'Content-Type': 'application/json;charset=utf-8',
+  'Access-Control-Allow-Origin': '*'
+};
 const whereSpentInput = document.getElementById('where-spent');
 const howMuchSpentInput = document.getElementById('how-much-spent');
 const buttonAddEl = document.getElementById('button-add-el');
@@ -7,7 +11,7 @@ let items;
 
 const render = collection => {
   mainList.innerHTML = '';
-  collection.forEach((item, index) => {
+  collection.forEach(item => {
     const li = document.createElement('li');
     let whereSpentSpan;
     if (item.isWhereEdit) {
@@ -18,18 +22,21 @@ const render = collection => {
       window.setTimeout(() => whereSpentSpan.focus(), 0);
     } else {
       whereSpentSpan = document.createElement('span');
+      whereSpentSpan.setAttribute('title', item.whereSpent)
       whereSpentSpan.innerHTML = item.whereSpent;
       whereSpentSpan.addEventListener('dblclick', () => onDblClickWhere(item))
     }
     let howMuchSpentSpan;
     if (item.isHowMuchEdit) {
       howMuchSpentSpan = document.createElement('input');
+      howMuchSpentSpan.setAttribute('type', 'number');
       howMuchSpentSpan.value = item.howMuchSpent;
       howMuchSpentSpan.addEventListener("keyup", (e) => onChangeTarget(e, item));
       howMuchSpentSpan.addEventListener("blur", (e) => editTarget(e, item));
       window.setTimeout(() => howMuchSpentSpan.focus(), 0);
     } else {
       howMuchSpentSpan = document.createElement('span');
+      howMuchSpentSpan.setAttribute('title', item.howMuchSpent)
       howMuchSpentSpan.innerHTML = `${item.howMuchSpent} p.`;
       howMuchSpentSpan.addEventListener('dblclick', () => onDblClickHowMuch(item))
     }
@@ -67,14 +74,11 @@ window.onload = async () => {
 }
 
 const onBtnAddClick = async () => {
-  if (whereSpentInput.value && typeof +howMuchSpentInput.value === 'number') {
+  if (whereSpentInput.value && howMuchSpentInput.value && typeof +howMuchSpentInput.value === 'number') {
     await request(
         'http://localhost:8000/createNewItem',
         'POST',
-        {
-          'Content-Type': 'application/json;charset=utf-8',
-          'Access-Control-Allow-Origin': '*'
-        },
+        HEADERS,
         {
           whereSpent: whereSpentInput.value,
           howMuchSpent: howMuchSpentInput.value
@@ -84,40 +88,39 @@ const onBtnAddClick = async () => {
   }
 }
 
-let functionName = onBtnAddClick;
+let btnAddOrSave = onBtnAddClick;
 
-const test = () => {
-  functionName();
+const onClickAddOrSave = async () => {
+  await btnAddOrSave();
 }
 
-buttonAddEl.addEventListener('click', () => test());
+buttonAddEl.addEventListener('click', () => onClickAddOrSave());
 
 const onBtnEditClick = item => {
   whereSpentInput.value = item.whereSpent;
   howMuchSpentInput.value = item.howMuchSpent;
   buttonAddEl.innerHTML = 'Сохранить';
-  functionName = () => editItem(item);
+  btnAddOrSave = () => editItem(item);
 }
 
 const editItem = async item => {
-  const itemId = item._id;
-  await request(
-      'http://localhost:8000/updateItem',
-      'PATCH',
-      {
-        'Content-Type': 'application/json;charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
-      },
-      {
-        _id: itemId,
-        whereSpent: whereSpentInput.value,
-        howMuchSpent: howMuchSpentInput.value
-      }
-  )
-  buttonAddEl.innerHTML = 'Добавить';
-  functionName = onBtnAddClick;
-  whereSpentInput.value = '';
-  howMuchSpentInput.value = '';
+  if (whereSpentInput.value && howMuchSpentInput.value && typeof +howMuchSpentInput.value === 'number') {
+    const itemId = item._id;
+    await request(
+        'http://localhost:8000/updateItem',
+        'PATCH',
+        HEADERS,
+        {
+          _id: itemId,
+          whereSpent: whereSpentInput.value,
+          howMuchSpent: howMuchSpentInput.value
+        }
+    )
+    buttonAddEl.innerHTML = 'Добавить';
+    btnAddOrSave = onBtnAddClick;
+    whereSpentInput.value = '';
+    howMuchSpentInput.value = '';
+  }
 }
 
 const onBtnDeleteClick = async item => {
@@ -125,10 +128,7 @@ const onBtnDeleteClick = async item => {
   await request(
       `http://localhost:8000/deleteItem?id=${itemId}`,
       'DELETE',
-      {
-        'Content-Type': 'application/json;charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
-      }
+      HEADERS,
   )
 }
 
@@ -156,29 +156,25 @@ async function editTarget(e, item) {
     await request(
         'http://localhost:8000/updateItem',
         'PATCH',
-        {
-          'Content-Type': 'application/json;charset=utf-8',
-          'Access-Control-Allow-Origin': '*'
-        },
+        HEADERS,
         {
           _id: itemId,
           whereSpent: e.target.value
         }
     )
   } else {
-    item.isHowMuchEdit = false;
-    const itemId = item._id;
-    await request(
-        'http://localhost:8000/updateItem',
-        'PATCH',
-        {
-          'Content-Type': 'application/json;charset=utf-8',
-          'Access-Control-Allow-Origin': '*'
-        },
-        {
-          _id: itemId,
-          howMuchSpent: e.target.value
-        }
-    )
+    if (e.target.value && typeof +e.target.value === 'number') {
+      item.isHowMuchEdit = false;
+      const itemId = item._id;
+      await request(
+          'http://localhost:8000/updateItem',
+          'PATCH',
+          HEADERS,
+          {
+            _id: itemId,
+            howMuchSpent: e.target.value
+          }
+      )
+    }
   }
 }
